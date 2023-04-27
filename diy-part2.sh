24d6800e9cc16ddade0bda42a4de74547450a73e
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2023-04-27 18:55:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -45,20 +45,20 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 
 #删除原默认主题
-rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-theme-bootstrap
-rm -rf package/lean/luci-theme-material
-rm -rf package/lean/luci-theme-netgear
+#rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-bootstrap
+#rm -rf package/lean/luci-theme-material
+#rm -rf package/lean/luci-theme-netgear
 rm -rf package/kenzo/luci-theme-ifit
 
 #下载主题luci-theme-argon
 git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
 git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
-git clone https://github.com/Joecaicai/luci-theme-argon.git package/lean/luci-theme-argon
+#git clone https://github.com/Joecaicai/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 修改luci-theme-argon_armygreen主题渐变色，16进制RGB
 #登录页面背景颜色+半透明
-sed -i 's/#f7fafc/rgba(134,176,197, .5)/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#sed -i 's/#f7fafc/rgba(134,176,197, .5)/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
 
 #渐变色开始
 #sed -i 's/#f9ffff/#80ABC3/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
