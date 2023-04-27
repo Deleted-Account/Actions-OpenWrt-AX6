caed468c55d715afeb741534fcf5a74ed8058e9b
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2023-04-27 18:06:25 +0800

    🤨

--- diy-part2.sh
+++ diy-part2.sh
@@ -79,7 +79,7 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 #----------------------------------------------#----------------------------------------------#
 
