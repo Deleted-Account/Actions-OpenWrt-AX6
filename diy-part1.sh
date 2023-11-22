bc2d3965f17d4983f9e064e1281c3bf838020901
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2023-11-23 01:20:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,6 @@
 
 # 添加插件源码
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+immortalwrt https://github.com/immortalwrt/packages
 # passwall依赖
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
