f3d2591175693d834b86fa11071ab82149bf400f
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2023-11-24 02:44:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -45,10 +45,10 @@
 
 
 #删除原默认主题
-rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-theme-bootstrap
-rm -rf package/lean/luci-theme-material
-rm -rf package/lean/luci-theme-netgear
+#rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-bootstrap
+#rm -rf package/lean/luci-theme-material
+#rm -rf package/lean/luci-theme-netgear
 #rm -rf package/kenzo/luci-theme-ifit
 
 #下载主题luci-theme-argon
