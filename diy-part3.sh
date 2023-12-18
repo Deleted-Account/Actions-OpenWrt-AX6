9553b2ace5cf9c5c5c6b48796988a5c252b02b76
Deleted-Account <48331538+Deleted-Account@users.noreply.github.com>
2023-12-18 16:48:18 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -10,8 +10,8 @@
 # Description: OpenWrt DIY script part 3 (Before Update feeds)
 #
 
-mv AX6/favicon.ico openwrt/package/lean/luci-theme-ifit/files/htdocs/favicon.ico
-mv AX6/logo.png openwrt/package/lean/luci-theme-ifit/files/htdocs/logo.png      
-mv AX6/bg/main_bg.jpg openwrt/package/lean/luci-theme-ifit/files/htdocs/images/main_bg.jpg
-mv AX6/bg/main_bg_5k.jpg openwrt/package/lean/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg
+# mv AX6/favicon.ico openwrt/package/lean/luci-theme-ifit/files/htdocs/favicon.ico
+# mv AX6/logo.png openwrt/package/lean/luci-theme-ifit/files/htdocs/logo.png      
+# mv AX6/bg/main_bg.jpg openwrt/package/lean/luci-theme-ifit/files/htdocs/images/main_bg.jpg
+# mv AX6/bg/main_bg_5k.jpg openwrt/package/lean/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg
 mv AX6/banner openwrt/package/base-files/files/etc/banner
